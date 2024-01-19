162b45a04345187d66515e79246c53ad0832e8d3
Rahul7549 <rksharma15700@gmail.com>
2024-01-19 20:17:35 +0530

    new project added

--- src/assets/ProjectData.js
+++ src/assets/ProjectData.js
@@ -4,17 +4,18 @@ import screenCareImg from './screenCare.jpeg'
 import covidTrakerImg from './covidTrakerApp.png'
 import netflixImg from './netflixClone.png'
 import weatherStatusImg from './weatherApplication.png'
+import RequestEase from './RequestEase.png'
 
 
 const projectCardData=[
     {
-        imgsrc:curatusImg,
-        title:'CURATUS:',
-        about:'Curatus is a repository of curated documents that are a result of technology or competitor analysis. A significant portion of these analyses are focused on patents and the findings from these analyses are typically shared over email and may vary from request to request. Curatus attempts to streamline this process by serving as a central repository where all these documents (suitably tagged, annotated and catalogued) and allowing for simple collaboration across all stakeholders.',
-        view:'https://curatus.biginfolabs.com/',
-        source:"https://biginfolabs.com/"
-        
+        imgsrc:RequestEase,
+        title:'RequestEase:',
+        about:'The application features a user-friendly architecture, comprising specific modules for administrators and regular users. Normal users can easily request service activation through an intuitive interface, simplifying navigation and selection of desired services. Additionally, administrators have access to all requests across the application, enabling them to approve or deny requests.',
+        view:'https://rahul7549.github.io/user-service.github.io/',
+        source:"https://github.com/Rahul7549/user-service.github.io"
     },
+    
     {
         imgsrc:screenCareImg,
         title:'SKIN CARE PRODUCT PERFORMANCE ANALYST:',
@@ -22,13 +23,7 @@ const projectCardData=[
         view:'',
         source:''
     },
-    {
-        imgsrc:netflixImg,
-        title:"NETFLIX CLONE:",
-        about:"Created a Netflix clone using ReactJS and consuming prebuilt TMDB APIs using axios and hosted it on Firebase Country",
-        view:"https://netflix-clone-e7320.web.app/",
-        source:'https://github.com/Rahul7549/netflix_clone'
-    },
+    
     {
         imgsrc:covidTrakerImg,
         title:'COVID TRACKER WEB APP:',
@@ -37,12 +32,29 @@ const projectCardData=[
         source:"https://github.com/Rahul7549/covid-tracker"
     },
     {
+        imgsrc:curatusImg,
+        title:'CURATUS:',
+        about:'Curatus acts as a central repository for curated documents, especially patent-focused technology or competitor analyses. It streamlines the sharing process via email by housing tagged, annotated, and cataloged documents. It fosters easy collaboration among stakeholders',
+        // about:'Curatus is a repository of curated documents that are a result of technology or competitor analysis. A significant portion of these analyses are focused on patents and the findings from these analyses are typically shared over email and may vary from request to request. Curatus attempts to streamline this process by serving as a central repository where all these documents (suitably tagged, annotated and catalogued) and allowing for simple collaboration across all stakeholders.',
+        view:'https://curatus.biginfolabs.com/',
+        source:"https://biginfolabs.com/"
+        
+    },
+    {
         imgsrc:weatherStatusImg,
         title:'Weather Application:',
         about:'Made ReactJs personal project that help to track the weather status in period of three hour cross the cities.',
         view:'https://rahul7549.github.io/weather-status/',
         source:"https://github.com/Rahul7549/weather-status"
     },
+    {
+        imgsrc:netflixImg,
+        title:"NETFLIX CLONE:",
+        about:"Created a Netflix clone using ReactJS and consuming prebuilt TMDB APIs using axios and hosted it on Firebase Country",
+        view:"https://netflix-clone-e7320.web.app/",
+        source:'https://github.com/Rahul7549/netflix_clone'
+    },
+    
     
 ]
 
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,56 +1,58 @@
 import './FooterStyle.css'
-import {FaHome,FaPhone,FaMailBulk, FaFacebook, FaLinkedin, FaInstagram,} from 'react-icons/fa'
+import { FaHome, FaPhone, FaMailBulk, FaFacebook, FaLinkedin, FaInstagram, } from 'react-icons/fa'
 
 export default function Footer() {
   return (
     <div className='footer'>
       <div className='footer-container'>
         <div className='left'>
-            <div className='location'>
-              
-              <FaHome size={20}
-                 style={{color:"#fff",marginRight:"2rem"}}/> 
-              <p>Dwarka Nagar, Bagalur Cross, Bengalore, 560064, India</p>
-              
-            </div>
+          <div className='location'>
 
-            <div className='phone'>
-                <h4>
-                <FaPhone size={20}
-                 style={{color:"#fff",marginRight:"2rem"}}/>
-                <span style={{marginRight:"1px"}}>+91</span> <span style={{marginLeft:"1px"}}>9110056292</span>
-                </h4>
-            </div>
-            <div className='email'>
-                <h4>
-                <FaMailBulk size={20}
-                 style={{color:"#fff",marginRight:"2rem"}}/>
-                Rksharma15700@gmail.com
-                </h4>
-            </div>
+            <FaHome size={20}
+              style={{ color: "#fff", marginRight: "2rem" }} />
+            <p>NFC, Taimoor Nagar, Delhi, 110025, India</p>
+
+          </div>
+
+          <div className='phone'>
+            <h4>
+              <FaPhone size={20}
+                style={{ color: "#fff", marginRight: "2rem" }} />
+              <span style={{ marginRight: "1px" }}>+91</span> <span style={{ marginLeft: "1px" }}>9110056292</span>
+            </h4>
+          </div>
+          <div className='email'>
+            <h4>
+              <FaMailBulk size={20}
+                style={{ color: "#fff", marginRight: "2rem" }} />
+              Rksharma15700@gmail.com
+            </h4>
+          </div>
         </div>
         <div className='right'>
-            <h4>About Me</h4>
-            <p>
-            Enthusiastic developer eager to contribute to team success through hard work, attention to detail and excellent
-            organizational skills. Motivated to learn new technologies grow and excel in the industry.
-            </p>
-            <div className='social'>
-              <a target='#' href='https://www.facebook.com/profile.php?id=100091796851448'>
+          <h4>About Me</h4>
+          <p>
+            Results-driven Software Developer with 2+ years of hands-on experience in dynamic web applications using
+            Angular. Proficient in full-stack development, specializing in Node.js, PostgreSQL, and React.js. Proven track
+            record of delivering high-quality solutions on time through effective collaboration with cross-functional
+            teams
+          </p>
+          <div className='social'>
+            <a target='#' href='https://www.facebook.com/profile.php?id=100091796851448'>
               <FaFacebook size={30}
-              style={{color:"#fff",marginRight:"1rem"}}/>
-              </a>
-              
+                style={{ color: "#fff", marginRight: "1rem" }} />
+            </a>
+
             <a target='#' href='https://www.linkedin.com/in/rahul-kumar-sharma-9bb998196/'>
-            <FaLinkedin size={30}
-              style={{color:"#fff",marginRight:"1rem",cursor:"pointer"}}/>
+              <FaLinkedin size={30}
+                style={{ color: "#fff", marginRight: "1rem", cursor: "pointer" }} />
             </a>
 
             <a target='#' href='https://www.instagram.com/accounts/onetap/?next=%2Frksharma15700%2F'>
-            <FaInstagram size={30}
-              style={{color:"#fff",marginRight:"1rem"}}/>
+              <FaInstagram size={30}
+                style={{ color: "#fff", marginRight: "1rem" }} />
             </a>
-            </div>
+          </div>
         </div>
       </div>
     </div>
